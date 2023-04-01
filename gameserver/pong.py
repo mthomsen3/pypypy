29d93cced2faabe7833761872e7e0a48ca5001c0
Terazax <mthomsen3@gmail.com>
2023-04-01 17:28:02 -0400

    Better collisions
    Not perfect, but better. Still cases where paddles can catch balls with sharp vertical angles. Might have to do with paddle speed vs ball speed?

--- gameserver/pong.py
+++ gameserver/pong.py
@@ -12,11 +12,11 @@ class Pong(Game):
         pass  # Implement Pong game start logic
 
     def update(self, player_actions):
-        # Update paddle positions based on player actions
+    # Update paddle positions based on player actions
         for player, action in player_actions.items():
-            player_index = self.players.index(player)  # Find the index of the player in the self.players list
+            player_index = self.players.index(player)
             if action is None:
-                continue  # No action for this player, skip to the next player
+                continue
             if action == "up":
                 self.state[f'paddle{player_index + 1}_y'] -= 0.02
             elif action == "down":
@@ -26,19 +26,17 @@ class Pong(Game):
         paddle_height = self.state['paddle_height']
         self.state['paddle1_y'] = max(0, min(1 - paddle_height, self.state['paddle1_y']))
         self.state['paddle2_y'] = max(0, min(1 - paddle_height, self.state['paddle2_y']))
+        
+        # Update ball position
+        self.state['ball_x'] += self.state['ball_velocity'][0]
+        self.state['ball_y'] += self.state['ball_velocity'][1]
 
         # Implement ball-wall collisions
         ball_diameter = self.state['ball_diameter']
         if self.state['ball_y'] <= 0 or self.state['ball_y'] + ball_diameter >= 1:
             self.state['ball_velocity'][1] = -self.state['ball_velocity'][1]
-            
-            
-        # Implement ball-paddle collisions
-        paddle1_x = self.state['paddle1_x']
-        paddle2_x = self.state['paddle2_x']
-        paddle_width = self.state['paddle_width']
-        ball_center_y = self.state['ball_y'] + (ball_diameter / 2)
 
+        # Implement ball-paddle collisions
         def calculate_ball_y_velocity(ball_center_y, paddle_y, paddle_height):
             relative_y = (ball_center_y - paddle_y) / paddle_height
             return (relative_y - 0.5) * 0.025  # Tweak this value to adjust the maximum Y velocity change
@@ -47,44 +45,51 @@ class Pong(Game):
             return (rect1[0] + rect1[2] > rect2[0] and rect1[0] < rect2[0] + rect2[2] and
                     rect1[1] < rect2[1] + rect2[3] and rect1[1] + rect1[3] > rect2[1])
             
-        def ball_paddle_collision_type(ball_rect, paddle_rect):
-            ball_center_x = ball_rect[0] + ball_rect[2] / 2
-            ball_center_y = ball_rect[1] + ball_rect[3] / 2
-            paddle_center_x = paddle_rect[0] + paddle_rect[2] / 2
-            paddle_center_y = paddle_rect[1] + paddle_rect[3] / 2
+        def ball_paddle_collision_response(ball_rect, paddle_rect):
+            overlap_left = paddle_rect[0] + paddle_rect[2] - ball_rect[0]
+            overlap_right = ball_rect[0] + ball_rect[2] - paddle_rect[0]
+            overlap_top = paddle_rect[1] + paddle_rect[3] - ball_rect[1]
+            overlap_bottom = ball_rect[1] + ball_rect[3] - paddle_rect[1]
 
-            dx = abs(ball_center_x - paddle_center_x)
-            dy = abs(ball_center_y - paddle_center_y)
+            min_overlap = min(overlap_left, overlap_right, overlap_top, overlap_bottom)
 
-            if dx > dy:
-                return 'side'
+            if min_overlap == overlap_left:
+                return 'left'
+            elif min_overlap == overlap_right:
+                return 'right'
+            elif min_overlap == overlap_top:
+                return 'top'
             else:
-                return 'top_bottom'
+                return 'bottom'
+
 
         ball_rect = [self.state['ball_x'], self.state['ball_y'], ball_diameter, ball_diameter]
-        paddle1_rect = [paddle1_x, self.state['paddle1_y'], paddle_width, paddle_height]
-        paddle2_rect = [paddle2_x, self.state['paddle2_y'], paddle_width, paddle_height]
+        paddle1_rect = [self.state['paddle1_x'], self.state['paddle1_y'], self.state['paddle_width'], paddle_height]
+        paddle2_rect = [self.state['paddle2_x'], self.state['paddle2_y'], self.state['paddle_width'], paddle_height]
 
+        # Detect if ball intersects paddle
+        paddle_collision = None
         if rects_intersect(ball_rect, paddle1_rect):
-            collision_type = ball_paddle_collision_type(ball_rect, paddle1_rect)
-            if collision_type == 'side':
-                self.state['ball_x'] = paddle1_x + paddle_width
-                self.state['ball_velocity'][0] = abs(self.state['ball_velocity'][0])
-                self.state['ball_velocity'][1] += calculate_ball_y_velocity(ball_center_y, self.state['paddle1_y'], paddle_height)
-            elif collision_type == 'top_bottom':
-                self.state['ball_velocity'][1] = -self.state['ball_velocity'][1]
+            paddle_collision = 'paddle1'
         elif rects_intersect(ball_rect, paddle2_rect):
-            collision_type = ball_paddle_collision_type(ball_rect, paddle2_rect)
-            if collision_type == 'side':
-                self.state['ball_x'] = paddle2_x - ball_diameter
-                self.state['ball_velocity'][0] = -abs(self.state['ball_velocity'][0])
-                self.state['ball_velocity'][1] += calculate_ball_y_velocity(ball_center_y, self.state['paddle2_y'], paddle_height)
-            elif collision_type == 'top_bottom':
+            paddle_collision = 'paddle2'
+            
+        if paddle_collision == 'paddle1':
+            collision_side = ball_paddle_collision_response(ball_rect, paddle1_rect)
+            if collision_side in ('left', 'right'):
+                self.state['ball_velocity'][0] = -self.state['ball_velocity'][0]
+                self.state['ball_velocity'][1] += calculate_ball_y_velocity(self.state['ball_y'] + (ball_diameter / 2), self.state['paddle1_y'], paddle_height)
+            elif collision_side in ('top', 'bottom'):
                 self.state['ball_velocity'][1] = -self.state['ball_velocity'][1]
 
-        # Update ball position
-        self.state['ball_x'] += self.state['ball_velocity'][0]
-        self.state['ball_y'] += self.state['ball_velocity'][1]
+        elif paddle_collision == 'paddle2':
+            collision_side = ball_paddle_collision_response(ball_rect, paddle2_rect)
+            if collision_side in ('left', 'right'):
+                self.state['ball_velocity'][0] = -self.state['ball_velocity'][0]
+                self.state['ball_velocity'][1] += calculate_ball_y_velocity(self.state['ball_y'] + (ball_diameter / 2), self.state['paddle2_y'], paddle_height)
+            elif collision_side in ('top', 'bottom'):
+                self.state['ball_velocity'][1] = -self.state['ball_velocity'][1]
+
 
         # Implement scoring logic
         winning_score = self.state['winning_score']
